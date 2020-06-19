faaca915b88652b2d1727a8bdf082c5e84327e4c
YukariClaerwen <yukari.8239@gmail.com>
2020-06-19 12:38:27 +0700

    update css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,63 @@
+$(document).ready(function () {
+    'use strict';
+    
+     var c, currentScrollTop = 0,
+        navbar = $('nav').not('.my-subnav');
+  
+    $(window).scroll(function () {
+        var a = $(window).scrollTop();
+        var b = navbar.height();
+       
+        currentScrollTop = a;
+        navbar.find('.show').each(function(){
+            $(this).removeClass('show');
+        })
+        if (c < currentScrollTop && a > b + b) {
+            navbar.addClass("scrollUp").removeClass('scrollDown');
+            navbar.find("button").removeClass('btn-border');
+        } else if (c > currentScrollTop && !(a <= b)) {
+            navbar.addClass('scrollDown').removeClass("scrollUp");
+        } else if (a < b) {
+            navbar.removeClass('scrollDown');
+            navbar.find("button").addClass('btn-border');
+        }
+        c = currentScrollTop;
+    });
 
-$("input").each(function(){
-    if ($(this).val().trim() != "") {
-        $(this).addClass('has-val');
-      } else {
-        $(this).removeClass('has-val');
-      }
-    $(this).blur(function(){
+    navbar.find('.navbar-toggler').click(function(){
+        var menu = navbar.find('.navbar-collapse');
+        if (menu.hasClass('show')){
+            navbar.removeClass('scrollDown');
+        } else {
+            navbar.addClass('scrollDown');
+        }
+    })
+
+    // var nav_h = navbar.outerHeight();
+    // $('.blog-nav').next('div').css({
+    //   'margin-top': nav_h
+    // })
+    // $('.section-banner').css({
+    //   'padding-top': nav_h + 25
+    // })
+    // $('.sticky-top').css({
+    //   top: nav_h + 15
+    // })
+
+    
+    $("input").each(function(){
         if ($(this).val().trim() != "") {
             $(this).addClass('has-val');
-          } else {
+        } else {
             $(this).removeClass('has-val');
-          }
+        }
+        $(this).blur(function(){
+            if ($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            } else {
+                $(this).removeClass('has-val');
+            }
+        })
     })
-})
+});
+
